b2756b1c7c61a95ea90168646ed083ecb51c37c2
Anastasiia Metikova <a.meti2301@gmail.com>
2022-02-18 19:58:40 +0700

    Add Badge component

---
+++ src/components/Badge.jsx
@@ -0,0 +1,19 @@
+import classNames from "classnames";
+
+function Badge({color, onClick, className, size}) {
+  return (
+    <div
+      onClick={onClick}
+      className={
+      classNames(
+        "badge",
+        { [`badge_color_${color}`]: color },
+        { [`badge_size_${size}`]: size },
+        className
+      )
+    }
+    />
+  )
+}
+
+export default Badge;
